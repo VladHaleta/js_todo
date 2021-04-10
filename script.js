e8a20c7c3cc9342d7a4e74342a38cf8627053821
costa <kossguzenko@gmail.com>
2021-04-10 22:39:54 +0300

    fix infinit render

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const todosList = {
-
   data: [
     {
       id: 11341,
@@ -20,16 +19,16 @@ const todosList = {
       id: 27111,
       text: 'drink coffee',
       checked: false,
-    }
+    },
   ],
 
   root: document.querySelector('#root'),
   rootChecked: document.querySelector('#root-checked'),
 
   render: function () {
-    root.innerHTML = ''
-    this.rootChecked.innerHTML = ''
-    this.data.forEach(el =>{
+    root.innerHTML = '';
+    this.rootChecked.innerHTML = '';
+    this.data.forEach((el) => {
       const todo = `
       <li data-id=${el.id}>
         <input class='checkbox' type="checkbox" name="" id="chbox${el.id}" checked >
@@ -37,25 +36,23 @@ const todosList = {
         <button class='todo-move-top'>top</button>
         <button class='todo-del'>del</button>
         <button class='todo-move-down'>down</button>
-      </li>`
-      if(!el.checked) this.root.innerHTML += todo
-      if(el.checked) this.rootChecked.innerHTML += todo
-    })
+      </li>`;
+      if (!el.checked) this.root.innerHTML += todo;
+      if (el.checked) this.rootChecked.innerHTML += todo;
+    });
 
-    const chboxAdd = document.querySelectorAll('.checkbox')
-    chboxAdd.forEach(el=>{
+    const chboxAdd = document.querySelectorAll('.checkbox');
+    chboxAdd.forEach((el) => {
       //console.log(el.checked)
       let chboxId = el;
       let i = 0;
-      this.data.forEach(el =>{
-        if(chboxId.id.substr(5) == el.id){
+      this.data.forEach((el) => {
+        if (chboxId.id.substr(5) == el.id) {
           chboxId.checked = el.checked;
         }
-      })
-      el.addEventListener('click', () => this.removeCkbox(chboxId))
-      
-    })
-    
+      });
+      el.addEventListener('click', () => this.removeCkbox(chboxId));
+    });
 
     /*
     const sorting = document.querySelectorAll('option')
@@ -65,44 +62,38 @@ const todosList = {
     })
     
     */
-    
-    const sorting = document.querySelector('#sorting')
-    sorting.addEventListener('change', () => this.dataSort(sorting))
-    
-    
-    const btnsDel = document.querySelectorAll('.todo-del')
-    btnsDel.forEach(el=>{
-        //const todoType = el.className
-      const todoId = el.parentNode.getAttribute('data-id')
-      el.addEventListener('click', () => this.remove(todoId) )
-    })
 
-    const btnsTop = document.querySelectorAll('.todo-move-top')
-    btnsTop.forEach(el=>{
-      const todoId = el.parentNode.getAttribute('data-id')
-      let copy = el.parentNode.previousElementSibling
-      let todoNextId 
-      if(copy) todoNextId = copy.getAttribute('data-id')
-      el.addEventListener('click', () => this.moveTop(todoId,todoNextId) )
-    })
+    const btnsDel = document.querySelectorAll('.todo-del');
+    btnsDel.forEach((el) => {
+      //const todoType = el.className
+      const todoId = el.parentNode.getAttribute('data-id');
+      el.addEventListener('click', () => this.remove(todoId));
+    });
 
-    const btnsDown = document.querySelectorAll('.todo-move-down')
-    btnsDown.forEach(el=>{
-      const todoId = el.parentNode.getAttribute('data-id')
-      let copy = el.parentNode.nextElementSibling
-      let todoNextId 
-      if(copy) todoNextId = copy.getAttribute('data-id')
-      el.addEventListener('click', ()=> this.moveDown(todoId,todoNextId) )
-    })
+    const btnsTop = document.querySelectorAll('.todo-move-top');
+    btnsTop.forEach((el) => {
+      const todoId = el.parentNode.getAttribute('data-id');
+      let copy = el.parentNode.previousElementSibling;
+      let todoNextId;
+      if (copy) todoNextId = copy.getAttribute('data-id');
+      el.addEventListener('click', () => this.moveTop(todoId, todoNextId));
+    });
+
+    const btnsDown = document.querySelectorAll('.todo-move-down');
+    btnsDown.forEach((el) => {
+      const todoId = el.parentNode.getAttribute('data-id');
+      let copy = el.parentNode.nextElementSibling;
+      let todoNextId;
+      if (copy) todoNextId = copy.getAttribute('data-id');
+      el.addEventListener('click', () => this.moveDown(todoId, todoNextId));
+    });
   },
 
   dataSort: function () {
-    console.log("yo")
-    
+    console.log('yo');
 
-    if(sorting.selectedIndex == 2){
-      /*
-      this.data.sort(function(a,b){
+    if (sorting.selectedIndex == 2) {
+      this.data.sort(function (a, b) {
         if (a.text > b.text) {
           return 1;
         }
@@ -111,105 +102,90 @@ const todosList = {
         }
         // a должно быть равным b
         return 0;
-      })
-      todosList.render()  
-      */
-     console.log("hyo")
-    }
+      });
+      todosList.render();
 
-    
-    
-    
+      console.log('hyo');
+    }
   },
 
   removeCkbox: function (event) {
-    this.data.forEach(el =>{
-      if(event.id.substr(5) == el.id) el.checked = !el.checked
-      todosList.render()
-    })
+    this.data.forEach((el) => {
+      if (event.id.substr(5) == el.id) el.checked = !el.checked;
+    });
 
+    todosList.render();
   },
 
   add: function (event) {
-    event.preventDefault()
-    const input = document.querySelector('.form-input')
+    event.preventDefault();
+    const input = document.querySelector('.form-input');
     const obj = {
       id: Math.floor(Math.random() * 100000) + 10000,
       text: input.value,
       checked: false,
-    }
+    };
 
-    todosList.data.unshift(obj)
-    todosList.render()
-    input.value = ''
+    todosList.data.unshift(obj);
+    todosList.render();
+    input.value = '';
   },
 
-
-  remove: function(id){
-    let i = 0
-    this.data.forEach(el =>{
-        if(el.id == id) this.data.splice(i,1)
-        i++
-    })
-    todosList.render()
+  remove: function (id) {
+    let i = 0;
+    this.data.forEach((el) => {
+      if (el.id == id) this.data.splice(i, 1);
+      i++;
+    });
+    todosList.render();
   },
 
-  moveTop: function(id, prevId){
-    let idIndex
-    let idIndexPrev
-    let i = 0
-    this.data.forEach(el =>{
-      if(el.id == prevId) idIndexPrev = i
-      if(el.id == id) idIndex = i
-      if(el.id == id){
-        if(idIndex  && idIndexPrev >= 0 ){
-          let copyTodoElem = this.data[idIndex]
-          this.data[idIndex] = this.data[idIndexPrev]
-          this.data[idIndexPrev] = copyTodoElem
+  moveTop: function (id, prevId) {
+    let idIndex;
+    let idIndexPrev;
+    let i = 0;
+    this.data.forEach((el) => {
+      if (el.id == prevId) idIndexPrev = i;
+      if (el.id == id) idIndex = i;
+      if (el.id == id) {
+        if (idIndex && idIndexPrev >= 0) {
+          let copyTodoElem = this.data[idIndex];
+          this.data[idIndex] = this.data[idIndexPrev];
+          this.data[idIndexPrev] = copyTodoElem;
         }
       }
-      i++
-    })
-    todosList.render()
+      i++;
+    });
+    todosList.render();
   },
 
-  moveDown: function(id, nextId){
-    let idIndex
-    let idIndexNext 
-    let i = 0
-    this.data.forEach(el =>{
-      if(el.id == id) idIndex = i
-      if(el.id == nextId) idIndexNext = i
-        if(idIndex >= 0 && idIndexNext ){
-          let copyTodoElem = this.data[idIndex]
-          this.data[idIndex] = this.data[idIndexNext]
-          this.data[idIndexNext] = copyTodoElem
-          idIndex = undefined
-          idIndexNext = undefined
-        }
-        i++
-    })
-    todosList.render()
+  moveDown: function (id, nextId) {
+    let idIndex;
+    let idIndexNext;
+    let i = 0;
+    this.data.forEach((el) => {
+      if (el.id == id) idIndex = i;
+      if (el.id == nextId) idIndexNext = i;
+      if (idIndex >= 0 && idIndexNext) {
+        let copyTodoElem = this.data[idIndex];
+        this.data[idIndex] = this.data[idIndexNext];
+        this.data[idIndexNext] = copyTodoElem;
+        idIndex = undefined;
+        idIndexNext = undefined;
+      }
+      i++;
+    });
+    todosList.render();
   },
 
   start: function () {
-    const btn = document.querySelector('.form-btn')
-    btn.addEventListener('click', (event) => this.add(event))
+    const sorting = document.querySelector('#sorting');
+    sorting.addEventListener('change', () => this.dataSort(sorting));
+    const btn = document.querySelector('.form-btn');
+    btn.addEventListener('click', (event) => this.add(event));
 
+    todosList.render();
+  },
+};
 
-    todosList.render()
-  }
-}
-
-todosList.start()
-
-
-const a1 = () => console.log
-
-const a2 = () => (
-  console.log()
-)
-
-const a3 = () => {
-
-}
+todosList.start();
